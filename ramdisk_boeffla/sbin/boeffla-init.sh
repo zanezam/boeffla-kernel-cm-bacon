778e477bfe5ac5d7e370d2b2434db32b2f9b12fc
andip71 <andreasp@gmx.de>
2014-11-05 11:11:35 +0100

    Boeffla ramfs: Various changes and cleanup
    - switch on dynamic fsync as part of kernel default settings
    - removed ac charge rate control setting, default is now stock logic
    - implement one time adjustments
    - cleanup of areas which were for Samsung devices only

--- ramdisk_boeffla/sbin/boeffla-init.sh
+++ ramdisk_boeffla/sbin/boeffla-init.sh
@@ -3,7 +3,7 @@
 # *****************************
 # Bacon Cyanogenmod 11 version
 #
-# V0.1
+# V0.2
 # *****************************
 
 # define basic kernel configuration
@@ -102,33 +102,11 @@
 # remove any obsolete Boeffla-Config V2 startconfig done file
 	/sbin/busybox rm -f $BOEFFLA_STARTCONFIG_DONE
 
-# Custom boot animation support
-	# Implementation 1
-	#if [ -f /data/local/bootanimation.zip ] || [ -f /system/media/bootanimation.zip ]; then
-	#		echo $(date) Playing custom boot animation >> $BOEFFLA_LOGFILE
-	#		/system/bin/bootanimation &
-	#else
-	#		echo $(date) Playing Samsung stock boot animation >> $BOEFFLA_LOGFILE
-	#		/system/bin/samsungani &
-	#fi
-
-	# Implementation 2
-	#if [ -f /data/local/bootanimation.zip ] || [ -f /system/media/bootanimation.zip ]; then
-	#		echo $(date) Playing custom boot animation >> $BOEFFLA_LOGFILE
-	#		/sbin/bootanimation &
-	#else
-	#		echo $(date) Playing Samsung stock boot animation >> $BOEFFLA_LOGFILE
-	#		/system/bin/bootanimation &
-	#fi
-
-# boeffla sound change delay (only for Samsung Kernels)
-	# echo "200000" > /sys/class/misc/boeffla_sound/change_delay
-	# echo $(date) Boeffla-Sound change delay set to 200 ms >> $BOEFFLA_LOGFILE
-
 # Apply Boeffla-Kernel default settings
 
-	# Set AC charging rate default
-	echo "1500" > /sys/kernel/charge_levels/charge_level_ac
+	# Sdcard buffer tweaks default to 1024 kb
+	echo 1024 > /sys/block/mmcblk0/bdi/read_ahead_kb
+	/sbin/busybox sync
 
 	# Ext4 tweaks default to on
 	/sbin/busybox sync
@@ -137,9 +115,9 @@
 	mount -o remount,commit=20,noatime $DATA_DEVICE /data
 	/sbin/busybox sync
 
-	# Sdcard buffer tweaks default to 1024 kb
-	echo 1024 > /sys/block/mmcblk0/bdi/read_ahead_kb
-	#echo 256 > /sys/block/mmcblk1/bdi/read_ahead_kb
+	# dynamic fsync to on
+	echo 1 > /sys/kernel/dyn_fsync/Dyn_fsync_active
+	/sbin/busybox sync
 
 	echo $(date) Boeffla-Kernel default settings applied >> $BOEFFLA_LOGFILE
 
@@ -170,24 +148,11 @@
 	echo $(date) Rom boot trigger detected, waiting a few more seconds... >> $BOEFFLA_LOGFILE
 	/sbin/busybox sleep 15
 
-# Play sound for Boeffla-Sound compatibility
-#	echo $(date) Initialize sound system... >> $BOEFFLA_LOGFILE
-#	/sbin/tinyplay /res/misc/silence.wav -D 0 -d 0 -p 880
-
-# Deactivate Samsung standard zRam implementation, if any
-	# busybox swapoff /dev/block/zram0
-	# echo "1" > /sys/block/zram0/reset
-	# echo "0" > /sys/block/zram0/disksize
-	# echo $(date) Samsung standard zRam deactivated >> $BOEFFLA_LOGFILE
-
 # Interaction with Boeffla-Config app V2
 	# save original stock values for selected parameters
 	cat /sys/devices/system/cpu/cpu0/cpufreq/UV_mV_table > /dev/bk_orig_cpu_voltage
-#	cat /sys/class/misc/gpu_clock_control/gpu_control > /dev/bk_orig_gpu_clock
-#	cat /sys/class/misc/gpu_voltage_control/gpu_control > /dev/bk_orig_gpu_voltage
 	cat /sys/kernel/charge_levels/charge_level_ac > /dev/bk_orig_charge_level_ac
 	cat /sys/kernel/charge_levels/charge_level_usb > /dev/bk_orig_charge_level_usb
-#	cat /sys/kernel/charge_levels/charge_level_wireless > /dev/bk_orig_charge_level_wireless
 	cat /sys/module/lowmemorykiller/parameters/minfree > /dev/bk_orig_minfree
 	/sbin/busybox lsmod > /dev/bk_orig_modules
 
@@ -199,41 +164,21 @@
 		echo $(date) Startup configuration applied  >> $BOEFFLA_LOGFILE
 	else
 		echo $(date) "No startup configuration found"  >> $BOEFFLA_LOGFILE
+	fi
 
-		# If not, apply default Boeffla-Kernel zRam
-		# Enable total 1 GB zRam on 4 devices as default
-		# busybox swapoff /dev/block/zram0
-		# busybox swapoff /dev/block/zram1
-		# busybox swapoff /dev/block/zram2
-		# busybox swapoff /dev/block/zram3
-		# echo "1" > /sys/block/zram0/reset
-		# echo "1" > /sys/block/zram1/reset
-		# echo "1" > /sys/block/zram2/reset
-		# echo "1" > /sys/block/zram3/reset
-		# echo "262144000" > /sys/block/zram0/disksize
-		# echo "262144000" > /sys/block/zram1/disksize
-		# echo "262144000" > /sys/block/zram2/disksize
-		# echo "262144000" > /sys/block/zram3/disksize
-		# busybox mkswap /dev/block/zram0
-		# busybox mkswap /dev/block/zram1
-		# busybox mkswap /dev/block/zram2
-		# busybox mkswap /dev/block/zram3
-		# busybox swapon -p 2 /dev/block/zram0
-		# busybox swapon -p 2 /dev/block/zram1
-		# busybox swapon -p 2 /dev/block/zram2
-		# busybox swapon -p 2 /dev/block/zram3
-		# busybox sleep 0.5s
-		# busybox sync
-		# echo "80" > /proc/sys/vm/swappiness
-		# echo $(date) Boeffla default zRam activated >> $BOEFFLA_LOGFILE
+# Perform one-time adjustments directly after first boot of the kernel
+	ADJUSTMENT_FILE="/data/.boeffla/_adj_2.0b9"
+	if [ ! -f $ADJUSTMENT_FILE ]; then
+		# adjust charge rates to stock values + readahead to 1024
+		echo "0" > /sys/kernel/charge_levels/charge_level_ac
+		echo "0" > /sys/kernel/charge_levels/charge_level_usb
+		echo 1024 > /sys/block/mmcblk0/bdi/read_ahead_kb
+		/sbin/busybox touch $ADJUSTMENT_FILE
 	fi
 	
 # Turn off debugging for certain modules
-	#echo 0 > /sys/module/ump/parameters/ump_debug_level
-	#echo 0 > /sys/module/mali/parameters/mali_debug_level
 	echo 0 > /sys/module/kernel/parameters/initcall_debug
 	echo 0 > /sys/module/lowmemorykiller/parameters/debug_level
-	#echo 0 > /sys/module/earlysuspend/parameters/debug_mask
 	echo 0 > /sys/module/alarm/parameters/debug_mask
 	echo 0 > /sys/module/alarm_dev/parameters/debug_mask
 	echo 0 > /sys/module/binder/parameters/debug_mask
@@ -304,9 +249,6 @@
 
 		echo $(date) CWM reset zip copied >> $BOEFFLA_LOGFILE
 	fi
-
-# Disable knox
-	# pm disable com.sec.knox.seandroid
 
 # Finished
 	echo $(date) Boeffla-Kernel initialisation completed >> $BOEFFLA_LOGFILE
