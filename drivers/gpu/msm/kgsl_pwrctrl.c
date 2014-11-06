c5ac381c0b0c65736994b3cdd6d5c9be70a27a99
andip71 <andreasp@gmx.de>
2014-11-06 08:00:39 +0100

    cpufreq: Add slim governor

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -43,6 +43,10 @@
 #define INIT_UDELAY		200
 #define MAX_UDELAY		2000
 
+#ifdef CONFIG_CPU_FREQ_GOV_SLIM
+int graphics_boost = 6;
+#endif
+
 /* Number of jiffies for a full thermal cycle */
 #define TH_HZ			20
 
@@ -224,6 +228,10 @@ void kgsl_pwrctrl_pwrlevel_change(struct kgsl_device *device,
 	clk_set_rate(pwr->grp_clks[0], pwrlevel->gpu_freq);
 	trace_kgsl_pwrlevel(device, pwr->active_pwrlevel,
 			pwrlevel->gpu_freq);
+
+#ifdef CONFIG_CPU_FREQ_GOV_SLIM
+    graphics_boost = pwr->active_pwrlevel;
+#endif
 }
 EXPORT_SYMBOL(kgsl_pwrctrl_pwrlevel_change);
 
