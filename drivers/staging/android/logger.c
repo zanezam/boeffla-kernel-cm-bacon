8548f047e3a54967088625fa677f72d398c61887
andip71 <andreasp@gmx.de>
2014-11-16 14:49:26 +0100

    logger: Add sys fs kernel interface to configure Android logger
    - now logging can be switched on and off via /sys/kernel/logger_mode/logger_mode interface
    - default is 1 (on)

--- drivers/staging/android/logger.c
+++ drivers/staging/android/logger.c
@@ -26,6 +26,7 @@
 #include <linux/slab.h>
 #include <linux/time.h>
 #include "logger.h"
+#include "logger_interface.h"
 
 #include <asm/ioctls.h>
 
@@ -400,6 +401,10 @@ static void do_write_log(struct logger_log *log, const void *buf, size_t count)
 {
 	size_t len;
 
+	// if logger mode is disabled, terminate instantly
+	if (logger_mode == 0)
+			return;
+
 	len = min(count, log->size - log->w_off);
 	memcpy(log->buffer + log->w_off, buf, len);
 
@@ -423,6 +428,10 @@ static ssize_t do_write_log_from_user(struct logger_log *log,
 {
 	size_t len;
 
+	// if logger mode is disabled, terminate instantly
+	if (logger_mode == 0)
+			return 0;
+			
 	len = min(count, log->size - log->w_off);
 	if (len && copy_from_user(log->buffer + log->w_off, buf, len))
 		return -EFAULT;
