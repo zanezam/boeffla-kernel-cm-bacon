398f5c53eadbb3aeaaf0823f21b366a1c55e3880
andip71 <andreasp@gmx.de>
2015-04-14 09:38:39 +0200

    Revert "netlink/wcnss: Squashed commits to provide foundation for wifi drivers LA.BF.1.1.1_rb1.17"
    This reverts commit c5fe0a8a2b744c9f7a3edc7fd156120b490f4e9d.

--- drivers/net/wireless/wcnss/wcnss_vreg.c
+++ drivers/net/wireless/wcnss/wcnss_vreg.c
@@ -52,8 +52,6 @@ static int auto_detect;
 #define WCNSS_PMU_CFG_GC_BUS_MUX_SEL_TOP   BIT(5)
 #define WCNSS_PMU_CFG_IRIS_XO_CFG_STS      BIT(6) /* 1: in progress, 0: done */
 
-#define WCNSS_PMU_CFG_IRIS_RESET           BIT(7)
-#define WCNSS_PMU_CFG_IRIS_RESET_STS       BIT(8) /* 1: in progress, 0: done */
 #define WCNSS_PMU_CFG_IRIS_XO_READ         BIT(9)
 #define WCNSS_PMU_CFG_IRIS_XO_READ_STS     BIT(10)
 
@@ -101,7 +99,7 @@ static struct vregs_info iris_vregs_pronto[] = {
 	{"qcom,iris-vddrfa", VREG_NULL_CONFIG, 1300000, 0,
 		1300000, 100000, NULL},
 	{"qcom,iris-vddpa",  VREG_NULL_CONFIG, 2900000, 0,
-		3350000, 515000, NULL},
+		3000000, 515000, NULL},
 	{"qcom,iris-vdddig", VREG_NULL_CONFIG, 1225000, 0,
 		1800000, 10000,  NULL},
 };
@@ -260,19 +258,6 @@ static int configure_iris_xo(struct device *dev, bool use_48mhz_xo, int on,
 				*iris_xo_set = WCNSS_XO_48MHZ;
 		}
 
-		writel_relaxed(reg, pmu_conf_reg);
-
-		/* Reset IRIS */
-		reg |= WCNSS_PMU_CFG_IRIS_RESET;
-		writel_relaxed(reg, pmu_conf_reg);
-
-		/* Wait for PMU_CFG.iris_reg_reset_sts */
-		while (readl_relaxed(pmu_conf_reg) &
-				WCNSS_PMU_CFG_IRIS_RESET_STS)
-			cpu_relax();
-
-		/* Reset iris reset bit */
-		reg &= ~WCNSS_PMU_CFG_IRIS_RESET;
 		writel_relaxed(reg, pmu_conf_reg);
 
 		/* Start IRIS XO configuration */
