6e843e4fef8ddb335688b21b510b51f761a589bd
andip71 <andreasp@gmx.de>
2015-07-29 14:25:20 +0200

    leds: Update Boeffla touchkey control driver to version 1.1.0

--- drivers/leds/boeffla_touchkey_control.c
+++ drivers/leds/boeffla_touchkey_control.c
@@ -1,7 +1,8 @@
 /*
- * Author: andip71, 21.09.2014
+ * 
+ * Boeffla touchkey control OnePlus One
  *
- * Version 1.0.0
+ * Author: andip71 (aka Lord Boeffla)
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -17,10 +18,21 @@
 /*
  * Change log:
  *
- * 1.0.0 (27.07.2015)
- *   - Initial version for OnePlus
+ * 1.1.0 (29.07.2015)
+ * 	- Add full kernel handling with configurable timeout
  *
  * 		Sys fs:
+ *
+ * 			/sys/class/misc/btk_control/btkc_timeout
+ *
+ * 				timeout in seconds from 1 - 30
+ * 				(0 = rom is taking care of the timeout)
+ *
+ *
+ * 1.0.0 (27.07.2015)
+ *	- Initial version
+ *
+ *		Sys fs:
  *
  * 			/sys/class/misc/btk_control/btkc_mode
  *
@@ -42,15 +54,74 @@
 #include <linux/module.h>
 #include <linux/miscdevice.h>
 #include <linux/platform_device.h>
+#include <linux/workqueue.h>
+#include <linux/notifier.h>
+#include <linux/kobject.h>
+#include <linux/lcd_notify.h>
 #include <linux/boeffla_touchkey_control.h>
 
 
 /*****************************************/
-// Variables
+// Declarations
 /*****************************************/
 
-int btkc_mode = MODE_NORMAL;
+int btkc_mode    = MODE_NORMAL;		// normal mode is default
+int btkc_timeout = TIMEOUT_DEFAULT;	// default is rom controlled timeout
+
 struct led_classdev *touch_led = NULL;
+
+static struct notifier_block lcd_notif;
+
+static void led_work_func(struct work_struct *unused);
+static DECLARE_DELAYED_WORK(led_work, led_work_func);
+
+
+/*****************************************/
+// internal functions
+/*****************************************/
+
+static void touchkey_led_enable(void)
+{
+	if (touch_led != NULL)
+		led_brightness_set(touch_led, LED_ON);
+}
+
+static void touchkey_led_disable(void)
+{
+	if (touch_led != NULL)
+		led_brightness_set(touch_led, LED_OFF);
+}
+
+static void led_work_func(struct work_struct *unused)
+{
+	pr_debug("Boeffla touch key control: timeout over, disable touchkey led");
+
+	// switch off LED and cancel any scheduled work
+	touchkey_led_disable();
+	cancel_delayed_work(&led_work);
+}
+
+static int lcd_notifier_callback(struct notifier_block *this,
+								unsigned long event, void *data)
+{
+	switch (event)
+	{
+		case LCD_EVENT_OFF_START:
+			pr_debug("Boeffla touch key control: screen off detected, disable touchkey led");
+			// switch off LED and cancel any scheduled work
+			touchkey_led_disable();
+			cancel_delayed_work(&led_work);
+			break;
+
+		case LCD_EVENT_ON_END:
+			break;
+
+		default:
+			break;
+	}
+
+	return 0;
+}
 
 
 /*****************************************/
@@ -59,35 +130,51 @@ struct led_classdev *touch_led = NULL;
 
 void btkc_touch(int x, int y)
 {
-	printk("Boeffla touch key control: touch detected %d %d\n", x, y);
+	pr_debug("Boeffla touch key control: touch detected %d %d\n", x, y);
 
-	// only control LED (activation) when touchkey only mode
+	// no special handling when not in touchkey_only mode
 	if (btkc_mode != MODE_TOUCHKEY_ONLY)
 		return;
 
-	// touch key with finger 0 received, check if a touch key was pressed
-	// if yes, enable touch key backlight
-	if ((y > TOUCHKEY_Y) && (touch_led != NULL))
-		led_brightness_set(touch_led, 255);
-}
+	// touch event with finger 0 received, check if a touch key was pressed;
+	// if yes, enable touch key led
+	if (y > TOUCHKEY_Y)
+	{
+		touchkey_led_enable();
 
+		// only if kernel should take over full control of the timeout,
+		// schedule the required delayed work to switch LED off again
+		if (btkc_timeout > 0)
+		{
+			cancel_delayed_work(&led_work);
+			schedule_delayed_work(&led_work, msecs_to_jiffies(btkc_timeout * 1000));
+		}
+	}
+}
 
 int btkc_block_touchkey_backlight(unsigned long state)
 {
+	pr_debug("Boeffla touch key control: Led sysfs hook - status %lu\n", state);
+
 	// normal mode = never block the LED
 	if (btkc_mode == MODE_NORMAL)
-		return 0;
+		return false;
 
-	// mode set to touchkey only or off = always block enabling the LED
+	// mode set to touchkey_only or off = always block enabling the LED
 	if (state != LED_OFF)
-		return 1;
+		return true;
 
-	return 0;
+	// if kernel has timeout control for touchkey_only mode = block LED completely
+	if ((btkc_mode == MODE_TOUCHKEY_ONLY) && (btkc_timeout > 0))
+		return true;
+
+	// do not block disabling the LED otherwise
+	return false;
 }
-
 
 void btkc_store_handle(struct led_classdev *led_cdev)
 {
+	// handle received by hook, store it
 	printk("Boeffla touch key control: Led handle received\n");
 	touch_led = led_cdev;
 	return;
@@ -103,7 +190,6 @@ static ssize_t btkc_mode_show(struct device *dev, struct device_attribute *attr,
 	return sprintf(buf, "Mode: %d\n", btkc_mode);
 }
 
-
 static ssize_t btkc_mode_store(struct device *dev, struct device_attribute *attr,
 						const char *buf, size_t count)
 {
@@ -116,18 +202,47 @@ static ssize_t btkc_mode_store(struct device *dev, struct device_attribute *attr
 	if (ret != 1)
 		return -EINVAL;
 
-	if ((val >= 0) || (val <= 2))
+	if ((val >= MODE_NORMAL) || (val <= MODE_OFF))
 	{
 		btkc_mode = val;
 
-		// disable touchkey backlight after every mode change
-		if (touch_led != NULL)
-			led_brightness_set(touch_led, 0);
+		// reset LED after every mode change
+		cancel_delayed_work(&led_work);
+		touchkey_led_disable();
 	}
 
 	return count;
 }
 
+
+static ssize_t btkc_timeout_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	return sprintf(buf, "Timeout [s]: %d\n", btkc_timeout);
+}
+
+static ssize_t btkc_timeout_store(struct device *dev, struct device_attribute *attr,
+						const char *buf, size_t count)
+{
+	unsigned int ret = -EINVAL;
+	unsigned int val;
+
+	// check data and store if valid
+	ret = sscanf(buf, "%d", &val);
+
+	if (ret != 1)
+		return -EINVAL;
+
+	if ((val >= TIMEOUT_MIN) || (val <= TIMEOUT_MAX))
+	{
+		btkc_timeout = val;
+
+		// reset LED after every timeout change
+		cancel_delayed_work(&led_work);
+		touchkey_led_disable();
+	}
+
+	return count;
+}
 
 static ssize_t btkc_version_show(struct device *dev, struct device_attribute *attr, char *buf)
 {
@@ -141,11 +256,13 @@ static ssize_t btkc_version_show(struct device *dev, struct device_attribute *at
 
 // define objects
 static DEVICE_ATTR(btkc_mode, S_IRUGO | S_IWUGO, btkc_mode_show, btkc_mode_store);
+static DEVICE_ATTR(btkc_timeout, S_IRUGO | S_IWUGO, btkc_timeout_show, btkc_timeout_store);
 static DEVICE_ATTR(btkc_version, S_IRUGO | S_IWUGO, btkc_version_show, NULL);
 
 // define attributes
 static struct attribute *btkc_attributes[] = {
 	&dev_attr_btkc_mode.attr,
+	&dev_attr_btkc_timeout.attr,
 	&dev_attr_btkc_version.attr,
 	NULL
 };
@@ -176,6 +293,11 @@ static int btk_control_init(void)
 		return 0;
 	}
 
+	// register callback for screen on/off notifier
+	lcd_notif.notifier_call = lcd_notifier_callback;
+	if (lcd_register_client(&lcd_notif) != 0)
+		pr_err("%s: Failed to register lcd callback\n", __func__);
+
 	// Print debug info
 	printk("Boeffla touch key control: driver version %s started\n", BTK_CONTROL_VERSION);
 	return 0;
@@ -184,6 +306,17 @@ static int btk_control_init(void)
 
 static void btk_control_exit(void)
 {
+	// remove boeffla touch key control device
+	sysfs_remove_group(&btkc_device.this_device->kobj,
+                           &btkc_control_group);
+
+	// cancel and flush any remaining scheduled work
+	cancel_delayed_work(&led_work);
+	flush_scheduled_work();
+
+	// unregister screen notifier
+	lcd_unregister_client(&lcd_notif);
+
 	// Print debug info
 	printk("Boeffla touch key control: driver stopped\n");
 }
