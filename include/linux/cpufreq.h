9ce9ae6a7a7e0c76e9111469748f079eecb1b969
andip71 <andreasp@gmx.de>
2015-08-07 17:11:41 +0200

    cpufreq: Fix a typo introduced for smartmax_eps governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -397,7 +397,7 @@ extern struct cpufreq_governor cpufreq_gov_wheatley;
 extern struct cpufreq_governor cpufreq_gov_smartmax;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartmax)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX_EPS)
-extern struct cpufreq_governor cpufreq_gov_smartmax_eüs;
+extern struct cpufreq_governor cpufreq_gov_smartmax_eps;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartmax_eps)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
 extern struct cpufreq_governor cpufreq_gov_intellimm;
