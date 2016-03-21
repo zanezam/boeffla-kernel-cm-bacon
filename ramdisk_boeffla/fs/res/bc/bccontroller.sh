7fe8b33cdfae4d6e62c8f1a0ad06ea635b29ae4d
andip71 <andreasp@gmx.de>
2016-03-21 13:03:32 +0100

    Boeffla ramfs: Temporary fix for broken toybox insmod command

--- ramdisk_boeffla/fs/res/bc/bccontroller.sh
+++ ramdisk_boeffla/fs/res/bc/bccontroller.sh
@@ -1295,7 +1295,7 @@ fi
 
 if [ "apply_cifs" == "$1" ]; then
 	if [ "1" == "$2" ]; then
-		insmod $LIBPATH/cifs.ko
+		busybox insmod $LIBPATH/cifs.ko
 	fi
 
 	if [ "0" == "$2" ]; then
@@ -1306,10 +1306,10 @@ fi
 
 if [ "apply_nfs" == "$1" ]; then
 	if [ "1" == "$2" ]; then
-		insmod $LIBPATH/sunrpc.ko
-		insmod $LIBPATH/auth_rpcgss.ko
-		insmod $LIBPATH/lockd.ko
-		insmod $LIBPATH/nfs.ko
+		busybox insmod $LIBPATH/sunrpc.ko
+		busybox insmod $LIBPATH/auth_rpcgss.ko
+		busybox insmod $LIBPATH/lockd.ko
+		busybox insmod $LIBPATH/nfs.ko
 	fi
 
 	if [ "0" == "$2" ]; then
@@ -1323,7 +1323,7 @@ fi
 
 if [ "apply_xbox" == "$1" ]; then
 	if [ "1" == "$2" ]; then
-		insmod $LIBPATH/xpad.ko
+		busybox insmod $LIBPATH/xpad.ko
 	fi
 
 	if [ "0" == "$2" ]; then
@@ -1334,8 +1334,8 @@ fi
 
 if [ "apply_exfat" == "$1" ]; then
 	if [ "1" == "$2" ]; then
-		insmod $LIBPATH/exfat_core.ko
-		insmod $LIBPATH/exfat_fs.ko
+		busybox insmod $LIBPATH/exfat_core.ko
+		busybox insmod $LIBPATH/exfat_fs.ko
 	fi
 
 	if [ "0" == "$2" ]; then
@@ -1347,7 +1347,7 @@ fi
 
 if [ "apply_usb_ethernet" == "$1" ]; then
 	if [ "1" == "$2" ]; then
-		insmod $LIBPATH/asix.ko
+		busybox insmod $LIBPATH/asix.ko
 		netcfg eth0 up
 		dhcpcd eth0
 		DNS=`getprop net.eth0.dns1`
@@ -1364,7 +1364,7 @@ fi
 
 if [ "apply_ntfs" == "$1" ]; then
 	if [ "1" == "$2" ]; then
-		insmod $LIBPATH/ntfs.ko
+		busybox insmod $LIBPATH/ntfs.ko
 	fi
 
 	if [ "0" == "$2" ]; then
