7bf916406ad52d3a36c5e72ece989e77861f1457
andip71 <andreasp@gmx.de>
2016-06-09 08:29:02 +0200

    Source drop: Merge code of branch stable/cm-13.0-ZNH2K in

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -261,10 +261,6 @@ extern int sysctl_tcp_default_init_rwnd;
 extern int sysctl_tcp_delack_seg;
 extern int sysctl_tcp_use_userconfig;
 
-/* sysctl variables for controlling various tcp parameters */
-extern int sysctl_tcp_delack_seg;
-extern int sysctl_tcp_use_userconfig;
-
 extern atomic_long_t tcp_memory_allocated;
 extern struct percpu_counter tcp_sockets_allocated;
 extern int tcp_memory_pressure;
@@ -1002,6 +998,8 @@ static const char *statename[]={
 extern void tcp_set_state(struct sock *sk, int state);
 
 extern void tcp_done(struct sock *sk);
+
+int tcp_abort(struct sock *sk, int err);
 
 static inline void tcp_sack_reset(struct tcp_options_received *rx_opt)
 {
