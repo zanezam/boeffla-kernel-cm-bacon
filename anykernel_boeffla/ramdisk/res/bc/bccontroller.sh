b958fb5a7dcb6012ce96f291ae9f4fbeeebb19c5
andip71 <andreasp@gmx.de>
2016-11-18 11:20:45 +0100

    Boeffla ramfs: Add filename template for kernel download

--- anykernel_boeffla/ramdisk/res/bc/bccontroller.sh
+++ anykernel_boeffla/ramdisk/res/bc/bccontroller.sh
@@ -13,7 +13,7 @@
 # ********************************
 
 # kernel specification (hardware; type; target; url)
-KERNEL_SPECS="bacon;cm;cm14.0;http://kernel.boeffla.de/bacon/boeffla-kernel-cm/"
+KERNEL_SPECS="bacon;cm;cm14.0;http://kernel.boeffla.de/bacon/boeffla-kernel-cm/;boeffla-kernel-#VERSION#-CM14.1-bacon-anykernel.recovery.zip"
 
 # kernel features 
 # (1=enable-busybox,2=enable-frandom,3=wipe-cache,4=disable-zram-control)
